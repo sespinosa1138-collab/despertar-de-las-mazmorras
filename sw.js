3f1c7ced1587432ba8f61c18d9e442ec8ebac893
sespinosa1138-collab <sespinosa1138@ielaunidad.edu.co>
2026-08-25 16:46:43 -0500

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'torre-system-v4-cache-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+// Instalación del Service Worker y almacenamiento en caché
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        return cache.addAll(urlsToCache);
+      })
+  );
+  self.skipWaiting();
+});
+
+// Activación y limpieza de cachés antiguas
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+// Interceptación de peticiones para funcionamiento offline
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
